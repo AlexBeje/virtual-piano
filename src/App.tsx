b6e29f6133e855136bea933be4c2dbaff1846f36
Alex <alex.bejenaru93@gmail.com>
2020-11-08 00:45:20 +0100

    📦 NEW: Stylized the piano component.

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,33 @@ import Piano from "./components/piano/piano.component";
 function App() {
   const [pianoKeys] = useState({
     white: ["C", "D", "E", "F", "G", "A", "B"],
-    black: ["C-Sharp", "D-Sharp", "F-Sharp", "G-Sharp", "A-Sharp"],
+    black: ["C-Sharp", "D-Sharp", "", "F-Sharp", "G-Sharp", "A-Sharp"],
   });
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    console.log(e.currentTarget.textContent);
-    const audio = new Audio(`sounds/piano_${e.currentTarget.textContent}.mp3`);
-    audio.play();
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("👱", e.currentTarget.value);
+    const audio = new Audio(`sounds/piano_${e.currentTarget.value}.mp3`);
+    e.currentTarget.value && audio.play();
   };
 
   return (
-    <div className="App flex h-screen">
+    <div className="app flex h-screen">
       <div className="m-auto text-center">
-        <h1>Virtual Piano</h1>
+        <h1 className="app__title mb-10">VIRTUAL PIANO</h1>
         <Piano
           whitePianoKeys={pianoKeys.white}
           blackPianoKeys={pianoKeys.black}
           handleClick={handleClick}
         />
-        <div className="flex justify-between items-center">
-          <AiFillGithub />
-          <FiFigma />
-          <AiOutlineCopyrightCircle />
-          Alexandru Bejenaru
+        <div className="flex justify-between mt-5">
+          <div className="flex items-center">
+            <AiFillGithub className="mr-5" />
+            <FiFigma />
+          </div>
+          <div className="flex items-center">
+            <AiOutlineCopyrightCircle />
+            Alexandru Bejenaru
+          </div>
         </div>
       </div>
     </div>
--- src/components/piano/piano.component.tsx
+++ src/components/piano/piano.component.tsx
@@ -1,10 +1,43 @@
 import React, { ReactElement } from "react";
 
-interface pianoProps {
+import styled from "styled-components";
+
+type pianoProps = {
   whitePianoKeys: string[];
   blackPianoKeys: string[];
-  handleClick: (e: React.MouseEvent<HTMLDivElement>) => void;
-}
+  handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+// Styled components
+const WhiteKey = styled.button`
+  background-color: #f4f4f4;
+  width: 100px;
+  height: 300px;
+  border: 1px solid black;
+  transition: 0.3s;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`;
+
+// Styled components
+const BlackKey = styled.button<{ transparent?: boolean }>`
+  background-color: #0a0a0a;
+  width: ${(props) => (!props.transparent ? "50px" : "0")};
+  margin-left: ${(props) => (!props.transparent ? "0" : "50px")};
+  height: 200px;
+  margin-top: -300px;
+  margin-right: 50px;
+  transition: 0.3s;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  &:first-child {
+    margin-left: 75px;
+  }
+  &:hover {
+    background-color: #252525;
+  }
+`;
 
 function piano({
   whitePianoKeys,
@@ -12,25 +45,21 @@ function piano({
   handleClick,
 }: pianoProps): ReactElement {
   const whiteKeys = whitePianoKeys.map((key, i) => {
-    /* TODO: Styled component */
     return (
-      <div onClick={handleClick} key={key + i}>
-        {key}
-      </div>
+      <WhiteKey onClick={(e) => handleClick(e)} key={key + i} value={key} />
     );
   });
   const blackKeys = blackPianoKeys.map((key, i) => {
-    /* TODO: Styled component */
-    return (
-      <div onClick={handleClick} key={key + i}>
-        {key}
-      </div>
+    return key ? (
+      <BlackKey onClick={(e) => handleClick(e)} key={key + i} value={key} />
+    ) : (
+      <BlackKey transparent key={key + i} />
     );
   });
   return (
     <div>
-      {whiteKeys}
-      {blackKeys}
+      <div className="flex">{whiteKeys}</div>
+      <div className="flex">{blackKeys}</div>
     </div>
   );
 }
